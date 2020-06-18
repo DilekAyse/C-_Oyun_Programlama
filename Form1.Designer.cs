353ce1189e0f84ff972d04cd59d5caacbef16298
Dilek Ayşe Gün <58185677+DilekAyse@users.noreply.github.com>
2020-06-18 14:50:24 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace proje_yazokulu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.oyunalanı = new System.Windows.Forms.Panel();
+            this.Point_lbl = new System.Windows.Forms.Label();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.top = new System.Windows.Forms.PictureBox();
+            this.Raket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.oyunalanı.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Raket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // oyunalanı
+            // 
+            this.oyunalanı.BackColor = System.Drawing.Color.BurlyWood;
+            this.oyunalanı.Controls.Add(this.Point_lbl);
+            this.oyunalanı.Controls.Add(this.score_lbl);
+            this.oyunalanı.Controls.Add(this.top);
+            this.oyunalanı.Controls.Add(this.Raket);
+            this.oyunalanı.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.oyunalanı.Location = new System.Drawing.Point(0, 0);
+            this.oyunalanı.Name = "oyunalanı";
+            this.oyunalanı.Size = new System.Drawing.Size(284, 261);
+            this.oyunalanı.TabIndex = 0;
+            this.oyunalanı.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.playground_PreviewKeyDown);
+            // 
+            // Point_lbl
+            // 
+            this.Point_lbl.AutoSize = true;
+            this.Point_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.Point_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Point_lbl.Location = new System.Drawing.Point(169, 9);
+            this.Point_lbl.Name = "Point_lbl";
+            this.Point_lbl.Size = new System.Drawing.Size(52, 55);
+            this.Point_lbl.TabIndex = 3;
+            this.Point_lbl.Text = "0";
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.score_lbl.ForeColor = System.Drawing.Color.DarkRed;
+            this.score_lbl.Location = new System.Drawing.Point(3, 9);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(169, 55);
+            this.score_lbl.TabIndex = 2;
+            this.score_lbl.Text = "Score:";
+            // 
+            // top
+            // 
+            this.top.BackColor = System.Drawing.Color.LimeGreen;
+            this.top.Location = new System.Drawing.Point(32, 99);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(30, 30);
+            this.top.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.top.TabIndex = 1;
+            this.top.TabStop = false;
+            // 
+            // Raket
+            // 
+            this.Raket.BackColor = System.Drawing.Color.Black;
+            this.Raket.Location = new System.Drawing.Point(71, 229);
+            this.Raket.Name = "Raket";
+            this.Raket.Size = new System.Drawing.Size(150, 20);
+            this.Raket.TabIndex = 0;
+            this.Raket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.oyunalanı);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.oyunalanı.ResumeLayout(false);
+            this.oyunalanı.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Raket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel oyunalanı;
+        private System.Windows.Forms.PictureBox top;
+        private System.Windows.Forms.PictureBox Raket;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Label Point_lbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje_yazokulu
+{
+    public partial class Form1 : Form
+    {
+        public int speed_left = 20; //speed of the ball - topun hızı
+        public int speed_top = 20;
+        public int points = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None; //remove any border-herhangi bir sınırı kaldırmak    
+            this.TopMost = true;                         // Bring the form to the front-formu ön tarafa getir
+            this.Bounds = Screen.PrimaryScreen.Bounds;   //Make it fullscreen -tam ekran yap
+
+            Raket.Top = oyunalanı.Bottom - (oyunalanı.Bottom / 10); //Set the position of Racket-Raketin konumunu ayarla
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Raket.Left = Cursor.Position.X - (Raket.Width / 2); 
+            top.Left += speed_left;
+            top.Top += speed_top;
+
+            if (top.Bottom >= Raket.Top && top.Bottom <= Raket.Bottom && top.Left >= Raket.Left && top.Right <= Raket.Right)
+            {
+                speed_top += 2;
+                speed_left += 2;
+                speed_top = -speed_top;
+                points += 1;
+                Point_lbl.Text = points.ToString();
+
+
+
+            }
+            if (top.Left <= oyunalanı.Left)
+            {
+                speed_left = -speed_left;
+
+            }
+            if (top.Right >= oyunalanı.Right)
+            {
+
+                speed_left = -speed_left;
+
+            }
+            if (top.Top <= oyunalanı.Top)
+            {
+                speed_top = -speed_top;
+
+            }
+            if (top.Bottom >= oyunalanı.Bottom)
+            {
+                timer1.Enabled = false; //stop the game
+
+            }
+
+
+        }
+
+        private void playground_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) { this.Close(); } // press Escape to Quit
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
